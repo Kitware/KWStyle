ed0d330b1f918052e6bc148a94b44f3ab0bdaad5
Julien Jomier <jjomier@cs.unc.edu>
2006-04-24 12:22:19 -0400

    ENH: Not checking header for Review directory

--- Web/ctests/KWStyle-ITK-Review.cmake
+++ Web/ctests/KWStyle-ITK-Review.cmake
@@ -7,7 +7,7 @@ SET (CTEST_COMMAND
 "/projects/KWStyle/CMake/bin/ctest -D ExperimentalStart"
 "/projects/KWStyle/CMake/bin/ctest -D ExperimentalUpdate"
 "/projects/KWStyle/CMake/bin/ctest -D ExperimentalConfigure"
-"/projects/KWStyle/KWStyle-Linux/KWStyle -lesshtml -o /projects/KWStyle/KWStyle/Web/ITKOverwrite.txt -xml /projects/KWStyle/KWStyle/Web/ITK.kws.xml -html /projects/KWStyle/Examples/Insight/Review -kwsurl http://66.194.253.24/KWStyleExamples/Insight/Review -dart ${CTEST_BINARY_DIRECTORY} -1 1 -D /projects/KWStyle/KWStyle/Web/ITKReviewFiles.txt" 
+"/projects/KWStyle/KWStyle-Linux/KWStyle -lesshtml -o /projects/KWStyle/KWStyle/Web/ITKOverwrite.txt -xml /projects/KWStyle/KWStyle/Web/ITKReview.kws.xml -html /projects/KWStyle/Examples/Insight/Review -kwsurl http://66.194.253.24/KWStyleExamples/Insight/Review -dart ${CTEST_BINARY_DIRECTORY} -1 1 -D /projects/KWStyle/KWStyle/Web/ITKReviewFiles.txt" 
 "/projects/KWStyle/CMake/bin/ctest -D ExperimentalSubmit"
 )
 
