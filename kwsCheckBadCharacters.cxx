dd80fdc4373ab48612a8e58b30be366159fb6557
Julien Jomier <jjomier@cs.unc.edu>
2007-01-09 14:50:37 -0500

    ENH: Now reporting only one error per line

--- kwsCheckBadCharacters.cxx
+++ kwsCheckBadCharacters.cxx
@@ -31,23 +31,28 @@ bool Parser::CheckBadCharacters(bool checkComments)
   
   unsigned long pos = 0;
   std::string::const_iterator it = buffer.begin();
+  long int currentline = -1;
   while(it!= buffer.end())
     {
     if(((*it)<0) ||((*it)>127))
       {
-      Error error;
-      error.line = this->GetLineNumber(pos,!checkComments);
-      error.line2 = error.line;
-      error.number = BADCHARACTERS;
-      error.description = "Bad character";
-      m_ErrorList.push_back(error);
-      hasErrors = true;
+      long int linenum = this->GetLineNumber(pos,!checkComments);
+      if(linenum != currentline)
+        {
+        currentline = linenum;
+        Error error;
+        error.line = linenum;
+        error.line2 = error.line;
+        error.number = BADCHARACTERS;
+        error.description = "Bad character";
+        m_ErrorList.push_back(error);
+        hasErrors = true;
+        }
       }
     pos++;
     it++;
     }
 
-  
   return !hasErrors;
 }
 
