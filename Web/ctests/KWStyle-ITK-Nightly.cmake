f68afa4608ea26d9e1839bd65c3b9e508f52492f
Julien Jomier <jjomier@cs.unc.edu>
2009-01-22 13:25:32 -0500

    ENH: Moved to dash21

--- Web/ctests/KWStyle-ITK-Nightly.cmake
+++ Web/ctests/KWStyle-ITK-Nightly.cmake
@@ -1,32 +1,22 @@
-SET (CTEST_SOURCE_DIRECTORY "/projects/KWStyle/Insight")
-SET (CTEST_BINARY_DIRECTORY "/projects/KWStyle/Insight-Linux")
+SET (CTEST_SOURCE_DIRECTORY "/home/Dashboards/KWStyleBuilds/Insight")
+SET (CTEST_BINARY_DIRECTORY "/projects/KWStyle/KWStyleBuilds/Insight-Linux")
 
 SET (KWSTYLE_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/Utilities/KWStyle")
 
 # which ctest command to use for running the dashboard
 SET (CTEST_COMMAND  
-#"/DartClient/CMake/bin/ctest -D Nightly -A ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}"
-"/projects/KWStyle/CMake-Linux/bin/ctest -D NightlyStart"
-"/projects/KWStyle/CMake-Linux/bin/ctest -D NightlyUpdate"
-"/projects/KWStyle/CMake-Linux/bin/ctest -D NightlyConfigure"
-"/projects/KWStyle/KWStyle-Linux/KWStyle -lesshtml -o ${CTEST_SOURCE_DIRECTORY}/Utilities/KWStyle/ITKOverwrite.txt -xml ${CTEST_BINARY_DIRECTORY}/Utilities/KWStyle/ITK.kws.xml -html /projects/KWStyle/Examples/Insight/Nightly -kwsurl http://66.194.253.24/KWStyleExamples/Insight/Nightly -dart ${CTEST_BINARY_DIRECTORY} -1 1 -D ${CTEST_BINARY_DIRECTORY}/Utilities/KWStyle/ITKFiles.txt" 
-"/projects/KWStyle/CMake-Linux/bin/ctest -D NightlySubmit -A ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}"
-)
-
-# what cmake command to use for configuring this dashboard
-SET (CTEST_CMAKE_COMMAND 
-  "/projects/KWStyle/CMake-Linux/bin/cmake"
-  )
-
-# set any extra envionment varibles here
-SET (CTEST_ENVIRONMENT
+"ctest -D NightlyStart"
+"ctest -D NightlyUpdate"
+"ctest -D NightlyConfigure"
+"/home/Dashboards/KWStyle-Build/KWStyle -lesshtml -o ${CTEST_SOURCE_DIRECTORY}/Utilities/KWStyle/ITKOverwrite.txt -xml ${CTEST_BINARY_DIRECTORY}/Utilities/KWStyle/ITK.kws.xml -dart ${CTEST_BINARY_DIRECTORY} -1 1 -D ${CTEST_BINARY_DIRECTORY}/Utilities/KWStyle/ITKFiles.txt" 
+"ctest -D NightlySubmit -A ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}"
 )
 
 SET (CTEST_INITIAL_CACHE 
 "CMAKE_GENERATOR:INTERNAL=Unix Makefiles
 MAKE_MAKE_PROGRAM:FILEPATH=make
-SITE:STRING=insight.journal.kitware
+SITE:STRING=dash21.kitware
 BUILDNAME:STRING=KWStyle
-CVSCOMMAND:FILEPATH=/usr/bin/cvs
 ITK_USE_KWSTYLE:BOOL=ON
 ")
+
