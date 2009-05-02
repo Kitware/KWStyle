541c6b706e7f6bb6890d93af0a595a1b4bbed64e
Julien Jomier <jjomier@cs.unc.edu>
2009-05-02 10:39:15 -0400

    BUG: Wrong check for struct

--- kwsParser.cxx
+++ kwsParser.cxx
@@ -1272,12 +1272,12 @@ bool Parser::IsInStruct(size_t pos,const char* buffer) const
   while(b!=std::string::npos)
     {
     // The next character should be either a space or a {
-    if(b+1==buf.size())
+    if(b+6==buf.size())
       {
       return false;
       }
     
-    if(buf[b+1]!=' ' && buf[b+1]!='{')
+    if(buf[b+6]!=' ' && buf[b+6]!='{')
       {
       return false;
       }
