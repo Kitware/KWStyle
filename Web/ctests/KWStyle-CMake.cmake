d423bcc9abc622ab64b5d49116424674335ab82a
Dave Partyka <dave.partyka@kitware.com>
2009-11-04 10:19:08 -0500

    ENH: upgrade the version of cmake to 2.8-rc5.

--- Web/ctests/KWStyle-CMake.cmake
+++ Web/ctests/KWStyle-CMake.cmake
@@ -3,11 +3,11 @@ SET (CTEST_BINARY_DIRECTORY "/home/kitware/Dashboards/KWStyleBuilds/CMake-KWStyl
 
 # which ctest command to use for running the dashboard
 SET (CTEST_COMMAND  
-"/home/kitware/Dashboards/CMake-build/bin/ctest -D NightlyStart"
-"/home/kitware/Dashboards/CMake-build/bin/ctest -D NightlyUpdate"
-"/home/kitware/Dashboards/CMake-build/bin/ctest -D NightlyConfigure"
+"/home/kitware/Dashboards/cmake-2.8.0-rc5/bin/ctest -D NightlyStart"
+"/home/kitware/Dashboards/cmake-2.8.0-rc5/bin/ctest -D NightlyUpdate"
+"/home/kitware/Dashboards/cmake-2.8.0-rc5/bin/ctest -D NightlyConfigure"
 "/home/kitware/Dashboards/KWStyle-Build/KWStyle -lesshtml -xml ${CTEST_BINARY_DIRECTORY}/CMake.kws.xml -dart ${CTEST_BINARY_DIRECTORY} -1 1 -o ${CTEST_SOURCE_DIRECTORY}/Utilities/KWStyle/CMakeOverwrite.txt -D ${CTEST_BINARY_DIRECTORY}/CMakeKWSFiles.txt "
-"ctest -D NightlySubmit -A ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}"
+"/home/kitware/Dashboards/cmake-2.8.0-rc5/bin/ctest -D NightlySubmit -A ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}"
 )
 
 SET (CTEST_INITIAL_CACHE 
