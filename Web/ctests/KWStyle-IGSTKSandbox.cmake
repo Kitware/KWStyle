2d46dbf0ea3246b0486b20036ee7f0c17e77bfa1
Dave Partyka <dave.partyka@kitware.com>
2010-02-18 17:41:47 -0500

    ENH: update KWStyle scripts to use a different ctest.

--- Web/ctests/KWStyle-IGSTKSandbox.cmake
+++ Web/ctests/KWStyle-IGSTKSandbox.cmake
@@ -5,9 +5,9 @@ SET (KWSTYLE_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/Utilities/KWStyle")
 
 # which ctest command to use for running the dashboard
 SET (CTEST_COMMAND  
-"/home/kitware/Dashboards/CMake-build/bin/ctest -D NightlyStart"
-"/home/kitware/Dashboards/CMake-build/bin/ctest -D NightlyUpdate"
-"/home/kitware/Dashboards/CMake-build/bin/ctest -D NightlyConfigure"
+"/home/kitware/Dashboards/cmake-2.8.0-rc5/bin/ctest -D NightlyStart"
+"/home/kitware/Dashboards/cmake-2.8.0-rc5/bin/ctest -D NightlyUpdate"
+"/home/kitware/Dashboards/cmake-2.8.0-rc5/bin/ctest -D NightlyConfigure"
 "/home/kitware/Dashboards/KWStyle-Build/KWStyle -lesshtml -o ${KWSTYLE_DIRECTORY}/IGSTKOverwrite.txt -xml ${CTEST_BINARY_DIRECTORY}/IGSTK.kws.xml -dart ${CTEST_BINARY_DIRECTORY} -1 1 -D ${CTEST_BINARY_DIRECTORY}/IGSTKFiles.txt" 
 "ctest -D NightlySubmit -A ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}"
 )
--- Web/ctests/KWStyle-ITK-Nightly.cmake
+++ Web/ctests/KWStyle-ITK-Nightly.cmake
@@ -5,9 +5,9 @@ SET (KWSTYLE_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/Utilities/KWStyle")
 
 # which ctest command to use for running the dashboard
 SET (CTEST_COMMAND
-"/home/kitware/Dashboards/CMake-build/bin/ctest -D NightlyStart"
-"/home/kitware/Dashboards/CMake-build/bin/ctest -D NightlyUpdate"
-"/home/kitware/Dashboards/CMake-build/bin/ctest -D NightlyConfigure"
+"/home/kitware/Dashboards/cmake-2.8.0-rc5/bin/ctest -D NightlyStart"
+"/home/kitware/Dashboards/cmake-2.8.0-rc5/bin/ctest -D NightlyUpdate"
+"/home/kitware/Dashboards/cmake-2.8.0-rc5/bin/ctest -D NightlyConfigure"
 "/home/kitware/Dashboards/KWStyle-Build/KWStyle -lesshtml -o ${CTEST_SOURCE_DIRECTORY}/Utilities/KWStyle/ITKOverwrite.txt -xml ${CTEST_BINARY_DIRECTORY}/Utilities/KWStyle/ITK.kws.xml -dart ${CTEST_BINARY_DIRECTORY} -1 1 -D ${CTEST_BINARY_DIRECTORY}/Utilities/KWStyle/ITKFiles.txt"
 "ctest -D NightlySubmit -A ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}"
 )
