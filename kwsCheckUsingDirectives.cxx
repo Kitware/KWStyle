b109c29ccaa37acda3f5a2991013108b79a6c2fa
Saif Al-Dein M Helmy <saif.helmy@symbyo.com>
2014-05-27 12:51:34 +0300

    Refactored CheckUsingDirective(...) to not use regex

--- kwsCheckUsingDirectives.cxx
+++ kwsCheckUsingDirectives.cxx
@@ -12,6 +12,7 @@
 
 =========================================================================*/
 #include "kwsParser.h"
+#include <cctype>
 
 namespace kws {
 
@@ -22,22 +23,71 @@ bool Parser::CheckUsingDirectives(bool forbidUsingDirectives)
     return true;
 
   bool hasError = false;
-  const char * ptr = m_BufferNoComment.c_str();
-  kwssys::RegularExpression regex("using[ \r\n\t]+namespace[ \r\n\t]+[A-Za-z_]+[ \r\n\t]*;");
-  while(ptr && regex.find(ptr))
-    {
-    ptr = m_BufferNoComment.c_str() + regex.end();
-    if(this->IsBetweenQuote(regex.start(), false))
-      continue;
 
-    hasError = true;
-    Error error;
-    error.line = this->GetLineNumber(regex.start(), true);
-    error.line2 = error.line;
-    error.description = "Using namespace directive used.";
-    error.number = USING_DIRECTIVES;
-    m_ErrorList.push_back(error);
+  size_t usingPos = m_BufferNoComment.find("using");
+  while( usingPos != std::string::npos )
+    {
+    size_t tempIndex = usingPos += 5;//points right after "using"
+    if(this->IsBetweenQuote(usingPos, false))
+      {
+      usingPos = m_BufferNoComment.find("using", tempIndex);
+      continue;
+      }
+
+    //Munch whitespace (at least one character)
+    int whitespaceCharacterCount = 0;
+    while(tempIndex < m_BufferNoComment.size() && isspace(m_BufferNoComment[tempIndex]))
+      {
+      tempIndex++;
+      whitespaceCharacterCount++;
+      }
+    if( tempIndex >= m_BufferNoComment.size() || whitespaceCharacterCount < 1 )//file ended or not enough whitespace characters
+      return true;
+
+    //Munch "namespace"
+    if(m_BufferNoComment.substr(tempIndex, 9) != "namespace")
+      {
+      usingPos = m_BufferNoComment.find("using", tempIndex);
+      continue;
+      }
+    tempIndex += 9;//points right after "namespace"
+
+    //Munch whitespace (at least one character)
+    whitespaceCharacterCount = 0;
+    while(tempIndex < m_BufferNoComment.size() && isspace(m_BufferNoComment[tempIndex]))
+      {
+      tempIndex++;
+      whitespaceCharacterCount++;
+      }
+    if( tempIndex >= m_BufferNoComment.size() || whitespaceCharacterCount < 1 )//file ended or not enough whitespace characters
+      return true;
+
+    //Munch identifier
+    while(tempIndex < m_BufferNoComment.size() && (isalpha(m_BufferNoComment[tempIndex]) || m_BufferNoComment[tempIndex] == '_') )
+      tempIndex++;
+    if( tempIndex >= m_BufferNoComment.size() )//file ended
+      return true;
+
+    //Munch whitespace
+    while(tempIndex < m_BufferNoComment.size() && isspace(m_BufferNoComment[tempIndex]))
+      tempIndex++;
+    if( tempIndex >= m_BufferNoComment.size() )//file ended
+      return true;
+
+    if(m_BufferNoComment[tempIndex] == ';')//Match found for the full "using namespace [A-Za-z_]+;"
+      {
+      hasError = true;
+      Error error;
+      error.line = this->GetLineNumber(usingPos, true);
+      error.line2 = error.line;
+      error.description = "Using namespace directive used.";
+      error.number = USING_DIRECTIVES;
+      m_ErrorList.push_back(error);
+      }
+
+    usingPos = m_BufferNoComment.find("using", tempIndex);
     }
+
   return !hasError;
 }
 
