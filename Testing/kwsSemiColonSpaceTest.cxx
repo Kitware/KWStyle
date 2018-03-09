cf3b3e6d80936ae5f95171508c054eee51ff5590
Hans Johnson <hans-johnson@uiowa.edu>
2018-03-09 12:16:31 -0600

    ENH: Test to demonstrate valid C++11 false positive
    As ITK moves to C++11 this paradigm is showing up often as a
    false positive from KWStyle.

--- Testing/kwsSemiColonSpaceTest.cxx
+++ Testing/kwsSemiColonSpaceTest.cxx
@@ -9,8 +9,8 @@
   Copyright (c) Insight Software Consortium. All rights reserved.
   See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even 
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
@@ -106,7 +106,26 @@ int kwsSemiColonSpaceTest(int, char* [] )
     return EXIT_FAILURE;
     }
   std::cout << "[PASSED]" << std::endl;
- 
+
+  // Test for good syntax of initializer value return lists
+  buffer = "const int spacing{ 1 };";
+  parser.ClearErrors();
+  parser.SetBuffer(buffer);
+  parser.Check("SemicolonSpace","0");
+
+  std::cout << "Test for return value initializer lists: ";
+  errors = parser.GetErrors();
+  if(errors.size() > 0)
+    {
+    for(unsigned int i=0;i<errors.size();i++)
+      {
+      std::cout << errors[i].description << std::endl;
+      }
+    std::cout << "[FAILED]" << std::endl;
+    return EXIT_FAILURE;
+    }
+  std::cout << "[PASSED]" << std::endl;
+
   // Test for good syntax
   buffer = "class myclass\n{\nmyfunction(){};\n\n\n\n};";
   parser.ClearErrors();
@@ -183,8 +202,8 @@ int kwsSemiColonSpaceTest(int, char* [] )
     return EXIT_FAILURE;
     }
   std::cout << "[PASSED]" << std::endl;
-  
+
   std::cout << "[DONE]" << std::endl;
-  
+
   return 0;
 }
