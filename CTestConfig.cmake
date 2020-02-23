e34c90ade9e332e95e9df0edf9a00d7c64502fad
Hans Johnson <hans.j.johnson@gmail.com>
2020-02-23 08:58:08 -0600

    ENH: Update travis to include running the test suite
    The previous travis dashobard was compiling the code,
    but not running the test suite.  This resulted in
    green merge commits without running the tests, which
    gave a false positive result.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,7 +7,7 @@
 set(CTEST_PROJECT_NAME "KWStyle")
 set(CTEST_NIGHTLY_START_TIME "21:00:00 EST")
 
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=KWStyle")
 set(CTEST_DROP_SITE_CDASH TRUE)
